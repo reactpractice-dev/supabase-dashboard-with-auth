97683beb83b7b5e19524a769cab5641ca0fcdead
Corina Udrescu <corinutz@gmail.com>
2024-07-20 08:48:25 +0200

    Add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,21 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import SignUp from "./components/SignUp";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
+  },
+]);
+
 export default function App() {
-  return <div>Hello world!</div>;
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }

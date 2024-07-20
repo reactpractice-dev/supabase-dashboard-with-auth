c6ca0ff40b8b27fc232c7f15744d073e2f6728fb
Corina Udrescu <corinutz@gmail.com>
2024-07-20 08:41:24 +0200

    Add sign up

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,14 +1,45 @@
+import { useState } from "react";
 import supabase from "../supabase";
 
 const SignUp = () => {
-  const handleSubmit = () => {};
+  const [isRegistered, setIsRegistered] = useState(false);
+  const [error, setError] = useState(null);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const { data, error } = await supabase.auth.signUp({
+      email: formData.get("email"),
+      password: formData.get("password"),
+    });
+    console.log(data, error);
+    if (!error) {
+      setIsRegistered(true);
+    } else {
+      setError(error.message);
+    }
+  };
+
+  if (isRegistered) {
+    return (
+      <div className="m-10 text-center font-bold">
+        You have successfully registered! Please check your email to verify your
+        account.
+      </div>
+    );
+  }
 
   return (
-    <div>
+    <div className="w-96 m-auto">
       <h2 className="m-10 text-center text-2xl font-bold">
         Create a new account
       </h2>
-      <form onSubmit={handleSubmit} className="w-72 m-auto">
+      {error && (
+        <div className=" text-red-700 p-4 text-center" role="alert">
+          Error - {error}
+        </div>
+      )}
+      <form onSubmit={handleSubmit}>
         <div>
           <label>
             Email address
@@ -17,7 +48,7 @@ const SignUp = () => {
               type="email"
               autoComplete="email"
               required
-              className="w-full py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 mb-2"
+              className="w-full p-1.5 shadow-sm ring-1 ring-inset ring-gray-300 mb-2"
             />
           </label>
         </div>
@@ -30,7 +61,7 @@ const SignUp = () => {
               type="password"
               autoComplete="new-password"
               required
-              className="w-full rounded-md py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 mb-2"
+              className="w-full rounded-md p-1.5 shadow-sm ring-1 ring-inset ring-gray-300 mb-2"
             />
           </label>
         </div>
@@ -38,7 +69,7 @@ const SignUp = () => {
         <div>
           <button
             type="submit"
-            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="w-full rounded-md bg-indigo-600 px-3 py-1.5  text-white shadow-sm hover:bg-indigo-500 focus-visible:outline"
           >
             Sign up
           </button>
